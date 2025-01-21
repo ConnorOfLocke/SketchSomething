9e22c79d830a5303c994ea8c537d92ed79c0ad57
Connor Locke-Warburton <clocke555@gmail.com>
2025-01-21 18:43:25 +1100

    Fixed undefined text at the end

--- src/components/modals/Complete.js
+++ src/components/modals/Complete.js
@@ -3,7 +3,12 @@ import Modal from "./Modal";
 
 function CompleteModal({ steps, open, onConfirm }) {
   return (
-    <Modal open={open} className={classes.complete} onConfirm={onConfirm} confirmText={"Alright!"}>
+    <Modal
+      open={open}
+      className={classes.complete}
+      onConfirm={onConfirm}
+      confirmText={"Alright!"}
+    >
       <div className={classes.completeBorder}>
         <div className={classes.innerBorder}>
           <h1>SESSION COMPLETE</h1>
@@ -14,8 +19,8 @@ function CompleteModal({ steps, open, onConfirm }) {
                 if (step.type === "promptSet") {
                   return (
                     <li
-                      key={`${step.subjectName}_${step.promptSetIndex}`}
-                    >{`${step.promptsPerSet} ${step.subjectName}`}</li>
+                      key={`${step.subject}_${step.promptSetIndex}`}
+                    >{`${step.prompts} ${step.subject}`}</li>
                   );
                 }
                 return null;
